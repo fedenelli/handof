d5d784a383c45a38bfbba462859a623000f78ddc
Federico Nelli <fn.nelli@gmail.com>
2022-10-30 21:55:14 -0300

    webhook support

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 C = exports;
 
 C.config = {
+  /* Email Notifications */
+  mailEnabled: false,
   accessKeyId: 'YOUR SES ACCESS KEY',
   secretAccessKey: 'YOUR SES SECRET_KEY',
   region: 'SES REGION',
   from: 'FROM EMAIL',
-  email: 'EMAIL TO SEND ALERTS TO'
+  email: 'EMAIL TO SEND ALERTS TO',
+  /* Webhook Notifications */
+  webhookEnabled: true,
+  webhookUrl: '',
+  diskAlertMessage: '',
+  mongoAlertMessage: '', 
 };
